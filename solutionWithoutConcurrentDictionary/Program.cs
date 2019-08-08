1adb39ccf5cb05e1f26c74c4a5ee16ce287c8c1c
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-08 15:34:28 +0100

    Add project files.

---
+++ solutionWithoutConcurrentDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solutionWithoutConcurrentDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userinput = Console.ReadLine();
+            Dictionary<char, int> hashing = new Dictionary<char, int>();
+            foreach (var c in userinput)
+            {
+                if (!hashing.ContainsKey(c))
+                {
+                    hashing.Add(c, 1);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in hashing)
+            {
+                sb.Append(c.Key);
+            }
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
